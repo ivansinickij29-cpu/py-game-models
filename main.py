e567b7ec700830514db9c60c59655f039b63de51
Ivan Synytskyi <ivansinickij29@icloud.com>
2026-05-13 10:49:59 +0200

    fix2

--- main.py
+++ main.py
@@ -10,26 +10,29 @@ def main() -> None:
     for player_nickname, player_details in all_players_data.items():
         race_obj, _ = Race.objects.get_or_create(
             name=player_details["race"]["name"],
-            defaults={"description": player_details["race"].get("description", "")}
+            defaults={
+                "description": player_details["race"].get("description", "")
+            }
         )
 
         for skill_info in player_details["race"]["skills"]:
             Skill.objects.get_or_create(
                 name=skill_info["name"],
-                race=race_obj,  # Прив'язуємо скіл до об'єкта раси
+                race=race_obj,
                 defaults={"bonus": skill_info.get("bonus", "")}
             )
         guild_obj = None
-        if player_details.get("guild"):  # Перевіряємо, чи є ключ 'guild' і він не None
+        if player_details.get("guild"):
             guild_obj, _ = Guild.objects.get_or_create(
                 name=player_details["guild"]["name"],
-                defaults={"description": player_details["guild"].get("description")}  # description може бути null
+                defaults={
+                    "description": player_details["guild"].get("description")}
             )
 
         Player.objects.create(
-            nickname=player_nickname,  # Використовуємо ключ словника як нікнейм гравця
-            email=player_details['email'],
-            bio=player_details.get('bio', ''),
+            nickname=player_nickname,
+            email=player_details["email"],
+            bio=player_details.get("bio", ""),
             race=race_obj,
             guild=guild_obj,
         )
